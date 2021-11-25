448cb61c521765fc18b487d773a7ccb40c371d58
Souvik Saha Joy <souviksahajoy@gmail.com>
2021-11-26 02:12:27 +0600

    suspense v2

--- server/api/products.ts
+++ server/api/products.ts
@@ -1,9 +1,18 @@
 import { createClient } from '@supabase/supabase-js'
 
-export default async() => {
+export default async(req, res) => {
   // eslint-disable-next-line node/no-deprecated-api
   const { SUPABASE_URL, SUPABASE_KEY } = process.env
-  const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
-  const { data } = await supabase.from('products').select('*')
-  return { products: data }
+  try {
+    const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+    const { data } = await supabase.from('products').select('*')
+    return { products: data }
+  }
+  catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error)
+    res.writeHead(500, { 'Content-Type': 'application/json' })
+    res.write(JSON.stringify(error))
+    res.end()
+  }
 }
